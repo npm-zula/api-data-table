41cb3289939b16fb17e8956bb676e84cafb175df
Aashir Aftab <i200523@nu.edu.pk>
2023-03-28 17:16:10 +0500

    ver final

--- scripts.js
+++ scripts.js
@@ -1,7 +1,149 @@
+
+// FETCH DATA INITIALLY FROM API
+function fetchData() {
+    $.ajax({
+        url: "https://jsonplaceholder.typicode.com/users",
+        dataType: "json",
+        success: function (data) {
+            // initial table population
+            populateTable(data);
+
+            // sort table on header click
+            $("#id-header").click(function () {
+                sortTable("id", data);
+                populateTable(data);
+            });
+            $("#name-header").click(function () {
+                sortTable("name", data);
+                populateTable(data);
+            });
+            $("#username-header").click(function () {
+                sortTable("username", data);
+                populateTable(data);
+            });
+            $("#email-header").click(function () {
+                sortTable("email", data);
+                populateTable(data);
+            });
+            $("#phone-header").click(function () {
+                sortTable("phone", data);
+                populateTable(data);
+            });
+            $("#website-header").click(function () {
+                sortTable("website", data);
+                populateTable(data);
+            });
+        }
+    });
+}
+
+// POPULATE TABLE
+function populateTable(data) {
+    $("#table-body").empty();
+
+    $.each(data, function (index, value) {
+        var row = $("<tr>");
+        $("<td></td>").text(value.id).appendTo(row);
+        $("<td></td>").text(value.name).appendTo(row);
+        $("<td></td>").text(value.username).appendTo(row);
+        $("<td></td>").text(value.email).appendTo(row);
+        $("<td></td>").text(value.phone).appendTo(row);
+        $("<td></td>").text(value.website).appendTo(row);
+        $("<td></td>").append("<button class='btn btn-dark btn-sm delete-btn' data-id='" + value.id + "' '>Delete</button>").appendTo(row);
+        $("<td></td>").append("<button class='btn btn-success btn-sm edit-btn' data-id='" + value.id + "' '>Edit</button>").appendTo(row);
+        $("#table-body").append(row);
+    });
+
+    $(".delete-btn").click(function () {
+        var id = $(this).data("id");
+        deleteRecord(id, data);
+    });
+
+    $(document).on("click", ".edit-btn", handleEditClick);
+
+}
+
+// EDIT CLICK HANDLER
+function handleEditClick(event) {
+    var id = $(this).data("id");
+    var row = $(this).closest("tr");
+    var name = row.find("td:nth-child(2)").text();
+    var username = row.find("td:nth-child(3)").text();
+    var email = row.find("td:nth-child(4)").text();
+    var phone = row.find("td:nth-child(5)").text();
+    var website = row.find("td:nth-child(6)").text();
+
+    // fill form fields with row data
+    $("#form-container-update input[name='id']").val(id);
+    $("#form-container-update input[name='name']").val(name);
+    $("#form-container-update input[name='username']").val(username);
+    $("#form-container-update input[name='email']").val(email);
+    $("#form-container-update input[name='phone']").val(phone);
+    $("#form-container-update input[name='website']").val(website);
+
+    // $("#form-container-update").submit(function (event) {
+    //     event.preventDefault();
+    //     handleEditSubmit(id);
+    // });
+
+    // show edit form
+    $("#form-container-update").slideToggle();
+}
+
+// DELETE BUTTON HANDLER
+function deleteRecord(id, data) {
+    var confirmDelete = confirm("Are you sure you want to delete this record?");
+    if (confirmDelete) {
+        $.ajax({
+            url: "https://jsonplaceholder.typicode.com/users/" + id,
+            type: "DELETE",
+            success: function (result) {
+                data = data.filter(function (user) {
+                    return user.id !== id;
+                });
+                populateTable(data);
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.log(textStatus + ": " + errorThrown);
+            }
+        });
+    }
+}
+
+// SORT USING HEADERS
+var sortedColumn = null;
+var sortOrder = -1;
+
+function sortTable(column, data) {
+    sortedColumn = column;
+    sortOrder = -sortOrder;
+    data.sort(function (a, b) {
+        var comparison = 0;
+        if (a[column] > b[column]) {
+            comparison = 1;
+        } else if (a[column] < b[column]) {
+            comparison = -1;
+        }
+        return sortOrder * comparison;
+    });
+}
+
+
+
 $(document).ready(function () {
     fetchData()
 });
 
+// SHOW / HIDE FORM (ADDING FORM) TOGGLE
+$("#show-form").click(function () {
+    $("#form-container").slideToggle();
+    $(this).text(function (i, text) {
+        return text === "Show Form" ? "Hide Form" : "Show Form";
+    });
+});
+
+
+// ADD NEW RECORD FORM SUBMISSION
 $(document).ready(function () {
     $('#myForm').submit(function (event) {
         event.preventDefault();
@@ -35,20 +177,13 @@ $(document).ready(function () {
                 type: 'POST',
                 url: 'https://jsonplaceholder.typicode.com/users',
                 data: user,
-                success: function (response) {
-                    // append the table with the response data
-                    var newRow = '<tr><td>' + response.id + '</td><td>' + response.name + '</td><td>' + response.username + '</td><td>' + response.email + '</td><td>' + response.phone + '</td><td>' + response.website + '</td><td><button class="btn btn-dark btn-sm delete-btn" data-id=' + response.id + '>Delete</button></td></tr>';
-                    // $("<td></td>").append("<button class='btn btn-dark btn-sm delete-btn' data-id='" + value.id + "' '>Delete</button>").appendTo(newRow);
+                success: function (data) {
+                    // append the table with the data data
+                    var newRow = '<tr><td>' + data.id + '</td><td>' + data.name + '</td><td>' + data.username + '</td><td>' + data.email + '</td><td>' + data.phone + '</td><td>' + data.website + '</td><td><button class="btn btn-dark btn-sm delete-btn" data-id=' + data.id + '>Delete</button></td>' + '<td><button class="btn btn-success btn-sm edit-btn" data-id=' + data.id + '>Edit</button></td></tr>' ;
+                    // $("<td></td>").append("<button class='btn btn-success btn-sm edit-btn' data-id='" + value.id + "' '>Edit</button>").appendTo(newRow);
+                    
                     $('#table-body').append(newRow);
-
-                    // reset the form
                     $('#myForm')[0].reset();
-
-                    // $(".delete-btn").click(function () {
-                    //     var id = $(this).data("id");
-                    //     console.log(id)
-                    //     deleteRecord(id, response);
-                    // });
                 },
                 error: function (jqXHR, textStatus, errorThrown) {
                     alert('Error submitting form: ' + textStatus + ' - ' + errorThrown);
@@ -94,74 +229,6 @@ $(document).ready(function () {
     }
 });
 
-
-// toggle form on button click
-$("#show-form").click(function () {
-    $("#form-container").slideToggle();
-    $(this).text(function (i, text) {
-        return text === "Show Form" ? "Hide Form" : "Show Form";
-    });
-});
-
-
-
-
-var sortedColumn = null;
-var sortOrder = -1;
-
-function sortTable(column, data) {
-    sortedColumn = column;
-    sortOrder = -sortOrder;
-    data.sort(function (a, b) {
-        var comparison = 0;
-        if (a[column] > b[column]) {
-            comparison = 1;
-        } else if (a[column] < b[column]) {
-            comparison = -1;
-        }
-        return sortOrder * comparison;
-    });
-}
-
-function populateTable(data) {
-    $("#table-body").empty();
-
-    $.each(data, function (index, value) {
-        var row = $("<tr>");
-        $("<td></td>").text(value.id).appendTo(row);
-        $("<td></td>").text(value.name).appendTo(row);
-        $("<td></td>").text(value.username).appendTo(row);
-        $("<td></td>").text(value.email).appendTo(row);
-        $("<td></td>").text(value.phone).appendTo(row);
-        $("<td></td>").text(value.website).appendTo(row);
-        $("<td></td>").append("<button class='btn btn-dark btn-sm delete-btn' data-id='" + value.id + "' '>Delete</button>").appendTo(row);
-        $("<td></td>").append("<button class='btn btn-success btn-sm edit-btn' data-id='" + value.id + "' '>Edit</button>").appendTo(row);
-        $("#table-body").append(row);
-    });
-
-    $(".delete-btn").click(function () {
-        var id = $(this).data("id");
-        deleteRecord(id, data);
-    });
-
-    // // toggle form on button click
-    // $(".edit-btn").click(function () {
-    //     // $("#form-container-update").slideToggle();
-    //     handleEditClick
-
-    // });
-
-    // $(".edit-btn").click(function () {
-    //     var id = $(this).data("id");
-    //     editRecord(id, data);
-    // });
-
-    $(document).on("click", ".edit-btn", handleEditClick);
-    
-}
-
-$("#myForm-update").on("submit", handleEditSubmit);
-
 function handleEditSubmit(event) {
     event.preventDefault();
     var formData = $(this).serialize();
@@ -193,87 +260,4 @@ function updateRowData(id, data) {
 }
 
 
-
-// function to handle edit button click event
-function handleEditClick(event) {
-    var id = $(this).data("id");
-    var row = $(this).closest("tr");
-    var name = row.find("td:nth-child(2)").text();
-    var username = row.find("td:nth-child(3)").text();
-    var email = row.find("td:nth-child(4)").text();
-    var phone = row.find("td:nth-child(5)").text();
-    var website = row.find("td:nth-child(6)").text();
-
-    // fill form fields with row data
-    $("#form-container-update input[name='id']").val(id);
-    $("#form-container-update input[name='name']").val(name);
-    $("#form-container-update input[name='username']").val(username);
-    $("#form-container-update input[name='email']").val(email);
-    $("#form-container-update input[name='phone']").val(phone);
-    $("#form-container-update input[name='website']").val(website);
-
-    // $("#form-container-update").submit(function (event) {
-    //     event.preventDefault();
-    //     handleEditSubmit(id);
-    // });
-
-    // show edit form
-    $("#form-container-update").slideToggle();
-}
-
-
-function deleteRecord(id, data) {
-    var confirmDelete = confirm("Are you sure you want to delete this record?");
-    if (confirmDelete) {
-        $.ajax({
-            url: "https://jsonplaceholder.typicode.com/users/" + id,
-            type: "DELETE",
-            success: function (result) {
-                data = data.filter(function (user) {
-                    return user.id !== id;
-                });
-                populateTable(data);
-            },
-            error: function (jqXHR, textStatus, errorThrown) {
-                console.log(textStatus + ": " + errorThrown);
-            }
-        });
-    }
-}
-
-function fetchData() {
-    $.ajax({
-        url: "https://jsonplaceholder.typicode.com/users",
-        dataType: "json",
-        success: function (data) {
-            // initial table population
-            populateTable(data);
-
-            // sort table on header click
-            $("#id-header").click(function () {
-                sortTable("id", data);
-                populateTable(data);
-            });
-            $("#name-header").click(function () {
-                sortTable("name", data);
-                populateTable(data);
-            });
-            $("#username-header").click(function () {
-                sortTable("username", data);
-                populateTable(data);
-            });
-            $("#email-header").click(function () {
-                sortTable("email", data);
-                populateTable(data);
-            });
-            $("#phone-header").click(function () {
-                sortTable("phone", data);
-                populateTable(data);
-            });
-            $("#website-header").click(function () {
-                sortTable("website", data);
-                populateTable(data);
-            });
-        }
-    });
-}
+$("#myForm-update").on("submit", handleEditSubmit);
